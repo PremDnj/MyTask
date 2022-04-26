e34f2bf07d465401ec38189d5fc62f495ab18e50
PremDnj <104187047+PremDnj@users.noreply.github.com>
2022-04-26 13:34:33 +0530

    Update Task4.kt

--- Task4.kt
+++ Task4.kt
@@ -1,4 +1,4 @@
-package com.example.myunittesting
+
 
 import kotlinx.coroutines.*
 import kotlin.system.measureTimeMillis
@@ -76,4 +76,75 @@ suspend fun carInterior(): String {
     println("Install Interior ---> End")
     return "Done"
 }
+
+
+
+
+// ================================== New Code =======================================
+
+
+
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
+import kotlin.time.Duration.Companion.microseconds
+import kotlin.time.Duration.Companion.milliseconds
+
+suspend fun main() {
+    println("<----- Tesla Car Building Start ---->")
+    val takeTime = measureTimeMillis {
+        val job = CoroutineScope(Dispatchers.IO).launch {
+            buildCars()
+        }
+        job.join()
+    }
+    println("<----- Tesla Car Building  End ----->")
+    println("<----- Total time taken to build - ${takeTime.microseconds} ----->")
+
+}
+
+fun buildCars() = runBlocking {
+
+    repeat(10000) {
+        launch {
+            var drive = async { scarDrive() }
+            var batry = async { scarBatteries() }
+            var inter = async { scarInterior() }
+            var body = async { scarBody() }
+            var painting = async { scarPainting() }
+
+            println(body.await())
+            println(painting.await())
+            println(drive.await())
+            println(batry.await())
+            println(inter.await())
+            println("Car Done $it / 10000 and it takes ${it.milliseconds}")
+        }
+    }
+}
+
+suspend fun scarBody(): String {
+    delay(5000L)
+    return "Build Body of car ---> End"
+}
+
+suspend fun scarPainting(): String {
+    delay(5000L)
+    return "Car painting Done ---> End"
+}
+
+suspend fun scarDrive(): String {
+    delay(10000L)
+    return "Install Car DriveTrain ---> End"
+}
+
+suspend fun scarBatteries(): String {
+    delay(15000L)
+    return "Install Batteries ---> End"
+}
+
+suspend fun scarInterior(): String {
+    delay(15000L)
+    return "Install Interior ---> End"
+}
+
 
